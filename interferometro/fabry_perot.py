731b230d2eb3d836acc6318e05f96e9f16216a32
Alessio Ramirez <alessio@Mac-mini-di-Alessio.local>
2025-06-23 17:50:41 +0200

    interferometro update

--- interferometro/fabry_perot.py
+++ interferometro/fabry_perot.py
@@ -10,13 +10,20 @@ def int_fabry (cos, A, d) :
     lambd = 632.8e-9 
     return A + (2*d/lambd)*cos
 
+def int_fabry_perot (N, A, d):
+    lambd = 632.8e-9
+    return N * lambd / (2 * d) + A
+
 l = Measurement(128, 0.5, magnitude=-2)
 diam = [2.0, 3.6, 4.8, 5.7, 6.5]
-diam = Measurement(diam, 0.1, -2)
+diam = Measurement(diam, 0.1, -2, name='Diametro', unit='m')
 cos = np.cos(np.arctan(diam/2/l))
-k = Measurement ([5,4,3,2,1])
-fit = perform_fit(cos, k, int_fabry, method='odr')
-plot_fit(fit, show_plot=True, show_params=True)
+cos.name, cos.unit = ('$\\cos(\\theta)$', 'rad')
+N = Measurement ([5,4,3,2,1], name='$N$')
+fit = perform_fit(N, cos, int_fabry_perot, method='odr')
+plot_fit(fit, plot_residuals=True, title='', save_path='./grafici/fabry_perot_interferenza.pdf')
+latex_table_data(diam, cos, N, orientation='v', caption='Dati Figura di Interferenza (Fabry-Perot)')
+latex_table_fit(fit, orientation='v', caption='Risultati Interpolazione (Fabry-Perot)')
 
 #CALIBRAZIONE MICROMETRO 
 #nonio mosso sempre tra le stesse posizioni
